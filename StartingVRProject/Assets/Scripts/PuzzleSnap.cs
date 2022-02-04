61ab34dfa658c85851536061810751fede5d46ef
Daniel Gerblick <daniel.gerblick@gmail.com>
2022-02-03 22:59:36 -0500

    Better puzzle

--- StartingVRProject/Assets/Scripts/PuzzleSnap.cs
+++ StartingVRProject/Assets/Scripts/PuzzleSnap.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PuzzleSnap : OVRGrabbable {
 
     public GameObject prize;
-    public PuzzleSnap piece0;
     public int id = 0;
     public float maxAngle = Mathf.Cos(45.0f * Mathf.Deg2Rad);
     public float puzzleSnapOffset = 0.3f;
@@ -15,6 +14,7 @@ public class PuzzleSnap : OVRGrabbable {
     public PuzzleSnap east = null;
     public PuzzleSnap west = null;
     private float grabbedAt;
+    private float solvedCountdown;
 
     override public void GrabBegin(OVRGrabber hand, Collider grabPoint) {
         grabbedAt = Time.time;
@@ -38,18 +38,54 @@ public class PuzzleSnap : OVRGrabbable {
     }
 
     private void Complete() {
-        bool complete = id == 0 &&
-                        east != null && east.id == 3 &&
-                        south != null && south.id == 2 &&
-                        east.south != null && east.south.id == 1 &&
-                        south.east != null && south.east.id == 1;
-        if (complete) {
-            Vector3 midpoint = (transform.position +
-                                east.transform.position +
-                                south.transform.position +
-                                south.east.transform.position) / 4;
-            midpoint.y = (midpoint.x + midpoint.z) / 2;
-            Instantiate(prize, midpoint, transform.rotation);
+        bool[] processed = { false, false, false, false };
+        PuzzleSnap[] puzzle = { null, null, null, null };
+        processed[id] = true;
+        Stack<PuzzleSnap> stack = new Stack<PuzzleSnap>();
+        stack.Push(this);
+        while (stack.Count > 0) {
+            PuzzleSnap current = stack.Pop();
+            int[] correct;
+            switch (current.id) {
+                case 0:
+                    correct = new int[4] { -1, 2, 3, -1 };
+                    break;
+                case 1:
+                    correct = new int[4] { 3, -1, -1, 2 };
+                    break;
+                case 2:
+                    correct = new int[4] { 0, -1, 1, -1 };
+                    break;
+                case 3:
+                    correct = new int[4] { -1, 1, -1, 0 };
+                    break;
+                default:
+                    return;
+            }
+            PuzzleSnap[] actual = { current.north, current.south, current.east, current.west };
+            for (int i = 0; i < 4; i++) {
+                int testId = actual[i] == null ? -1 : actual[i].id;
+                if (correct[i] != testId) {
+                    Debug.Log(current.id + ": Failed on i=" + i + ", expecting " + correct[i] + ", got " + testId);
+                    return;
+                }
+                if (correct[i] != -1 && !processed[correct[i]]) {
+                    processed[correct[i]] = true;
+                    stack.Push(actual[i]);
+                }
+            }
+            puzzle[current.id] = current;
+        }
+        Vector3 spawnPoint = Vector3.zero;
+        for (int i = 0; i < 4; i++) {
+            if (!processed[i] || puzzle[i].isGrabbed)
+                return;
+            spawnPoint += puzzle[i].transform.position / 4;
+        }
+        spawnPoint.y += 1;
+        if (puzzle[0].solvedCountdown < 0.0f) {
+            Instantiate(prize, spawnPoint, transform.rotation);
+            puzzle[0].solvedCountdown = 1.0f;
         }
     }
 
@@ -80,15 +116,16 @@ public class PuzzleSnap : OVRGrabbable {
         if (other.tag != "PuzzlePiece")
             return;
         PuzzleSnap piece = other.GetComponent<PuzzleSnap>();
+        solvedCountdown -= Time.fixedDeltaTime;
 
         Vector3 nSnap = piece.transform.position + piece.transform.forward * puzzleSnapOffset;
         Vector3 sSnap = piece.transform.position - piece.transform.forward * puzzleSnapOffset;
         Vector3 eSnap = piece.transform.position + piece.transform.right * puzzleSnapOffset;
         Vector3 wSnap = piece.transform.position - piece.transform.right * puzzleSnapOffset;
 
-        if (this == piece.north && Vector3.Distance(nSnap, transform.position) < puzzleSnapMargin) {
+        if (!isGrabbed && this == piece.north && Vector3.Distance(nSnap, transform.position) < puzzleSnapMargin) {
             AlignTo(piece.gameObject, nSnap);
-        } else if (this == piece.east && Vector3.Distance(eSnap, transform.position) < puzzleSnapMargin) {
+        } else if (!isGrabbed && this == piece.east && Vector3.Distance(eSnap, transform.position) < puzzleSnapMargin) {
             AlignTo(piece.gameObject, eSnap);
         }
 
@@ -100,19 +137,19 @@ public class PuzzleSnap : OVRGrabbable {
         if (south == null && Vector3.Distance(nSnap, transform.position) < puzzleSnapMargin && AlignTo(piece.gameObject, nSnap)) {
             south = piece;
             piece.north = this;
-            piece0.Complete();
+            Complete();
         } else if (north == null && Vector3.Distance(sSnap, transform.position) < puzzleSnapMargin && AlignTo(piece.gameObject, sSnap)) {
             north = piece;
             piece.south = this;
-            piece0.Complete();
+            Complete();
         } else if (west == null && Vector3.Distance(eSnap, transform.position) < puzzleSnapMargin && AlignTo(piece.gameObject, eSnap)) {
             west = piece;
             piece.east = this;
-            piece0.Complete();
+            Complete();
         } else if (east == null && Vector3.Distance(wSnap, transform.position) < puzzleSnapMargin && AlignTo(piece.gameObject, wSnap)) {
             east = piece;
             piece.west = this;
-            piece0.Complete();
+            Complete();
         }
     }
 
