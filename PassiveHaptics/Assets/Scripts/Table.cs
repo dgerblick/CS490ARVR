930e5eb1a63a008bc93b38eb8a2983c0c0b26465
Daniel Gerblick <daniel.gerblick@gmail.com>
2022-02-16 14:26:32 -0500

    Table Calibration

--- PassiveHaptics/Assets/Scripts/Table.cs
+++ PassiveHaptics/Assets/Scripts/Table.cs
@@ -5,18 +5,49 @@ using UnityEngine;
 public class Table : MonoBehaviour {
 
     public float offset = -0.05f;
+    public float thickness = 0.025f;
 
     private bool _calibHeight = false;
     private OVRHand _activeHand;
     private CapsuleCollider[] _activeColliders;
+    private TableAnchor[] _anchors;
+    private GameObject _cube;
 
     private void Start() {
-    
+        _anchors = GetComponentsInChildren<TableAnchor>();
+        _cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        _cube.transform.SetParent(transform);
+        _cube.transform.localPosition = Vector3.zero;
+        _cube.transform.localRotation = Quaternion.identity;
+        _cube.transform.localScale = new Vector3(0, thickness, 0);
     }
 
-    private void Update() {
+    private void LateUpdate() {
         if (_calibHeight && _activeHand.enabled)
             CalibHeight();
+        else
+            Rescale();
+    }
+
+    private void Rescale() {
+        float minX = float.PositiveInfinity;
+        float minZ = float.PositiveInfinity;
+        float maxX = float.NegativeInfinity;
+        float maxZ = float.NegativeInfinity;
+        foreach (TableAnchor anchor in _anchors) {
+            minX = Mathf.Min(anchor.transform.localPosition.x, minX);
+            minZ = Mathf.Min(anchor.transform.localPosition.z, minZ);
+            maxX = Mathf.Max(anchor.transform.localPosition.x, maxX);
+            maxZ = Mathf.Max(anchor.transform.localPosition.z, maxZ);
+        }
+        Vector3 delta = new Vector3((maxX + minX) / 2, 0, (maxZ + minZ) / 2);
+        transform.Translate(delta);
+        _cube.transform.localScale = new Vector3(maxX - minX, thickness, maxZ - minZ);
+        foreach (TableAnchor anchor in _anchors) {
+            anchor.transform.localPosition = Vector3.Project(anchor.transform.localPosition, anchor.scaleVec);
+            anchor.transform.Translate(-delta);
+        }
+
     }
 
     private void CalibHeight() {
@@ -30,14 +61,18 @@ public class Table : MonoBehaviour {
         Quaternion rot = Quaternion.LookRotation(forward, Vector3.up);
         transform.SetPositionAndRotation(pos, rot);
 
-        if (_activeHand.GetFingerIsPinching(OVRHand.HandFinger.Index))
+        if (_activeHand.GetFingerIsPinching(OVRHand.HandFinger.Index)) {
             _calibHeight = false;
+            foreach (TableAnchor anchor in _anchors)
+                anchor.enabled = true;
+        }
     }
 
     public void StartCalibHeight(OVRHand hand) {
         _calibHeight = true;
         _activeHand = hand;
-        _activeColliders=hand.GetComponentsInChildren<CapsuleCollider>();
+        _activeColliders = hand.GetComponentsInChildren<CapsuleCollider>();
+        foreach (TableAnchor anchor in _anchors)
+            anchor.enabled = false;
     }
 }
-    
---
+++ PassiveHaptics/Assets/Scripts/TableAnchor.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableAnchor : MonoBehaviour {
+
+    public enum AnchorType {
+        Scale,
+    };
+    public AnchorType anchorType;
+    public Vector3 scaleVec;
+    public float pinchDist = 0.1f;
+    public OVRHand lHand;
+    public OVRHand rHand;
+
+    private OVRHand _activeHand = null;
+
+    private void Start() {
+        scaleVec = transform.localPosition.normalized;
+    }
+
+    private void Update() {
+        float lDist = Vector3.Distance(lHand.transform.position, transform.position);
+        float rDist = Vector3.Distance(rHand.transform.position, transform.position);
+        if (_activeHand == null && lDist < pinchDist)
+            _activeHand = lHand;
+        else if (_activeHand == null && rDist < pinchDist)
+            _activeHand = rHand;
+        
+        if (_activeHand != null && _activeHand.enabled && _activeHand.GetFingerIsPinching(OVRHand.HandFinger.Index))
+            transform.position = _activeHand.transform.position;
+        else
+            _activeHand = null;
+    }
+}
