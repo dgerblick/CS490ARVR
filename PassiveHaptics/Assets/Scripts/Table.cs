283fa1858a2d2ac564ae27a8e0e6b53d19322fa4
Daniel Gerblick <daniel.gerblick@gmail.com>
2022-02-18 20:18:42 -0500

    Cube Spawning for Redirect

--- PassiveHaptics/Assets/Scripts/Table.cs
+++ PassiveHaptics/Assets/Scripts/Table.cs
@@ -10,6 +10,7 @@ public class Table : MonoBehaviour {
         CalibSize,
         BugGameSpawn,
         BugGamePlay,
+        Redirect
     };
 
     public State state = State.None;
@@ -22,12 +23,16 @@ public class Table : MonoBehaviour {
     public Bug bugPrefab;
     public float bugSpawnTime = 10.0f;
     public float bugSpawnRate = 10.0f;
+    public float touchCubeOffset = 0.1f;
 
     private OVRHand _activeHand;
     private CapsuleCollider[] _activeColliders;
     private TableAnchor[] _anchors;
-    private GameObject _cube;
+    private GameObject _tableCube;
     private GameObject _button;
+    private GameObject _touchCubesParent;
+    private GameObject[] _touchCubes;
+    private BoxCollider _collider;
     private float _bugSpawnDelay;
     private float _bugSpawnCountdown;
     public List<Bug> _bugs;
@@ -40,15 +45,31 @@ public class Table : MonoBehaviour {
     private void Start() {
         _anchors = GetComponentsInChildren<TableAnchor>();
         _button = GetComponentInChildren<CustomButton>().transform.parent.gameObject;
+        _collider = GetComponent<BoxCollider>();
         _bugSpawnDelay = 0.0f;
         _bugSpawnCountdown = bugSpawnTime;
 
-        _cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        _cube.transform.SetParent(transform);
-        _cube.transform.localPosition = Vector3.zero;
-        _cube.transform.localRotation = Quaternion.identity;
-        _cube.transform.localScale = new Vector3(0, thickness, 0);
-        SetCubeSize();
+        _tableCube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        _tableCube.transform.SetParent(transform);
+        _tableCube.transform.localPosition = Vector3.zero;
+        _tableCube.transform.localRotation = Quaternion.identity;
+        _tableCube.transform.localScale = new Vector3(0, thickness, 0);
+        SetTableCubeSize();
+
+        _touchCubesParent = new GameObject();
+        _touchCubesParent.transform.SetParent(transform);
+        _touchCubesParent.transform.localPosition = Vector3.zero;
+        _touchCubesParent.transform.localRotation = Quaternion.identity;
+        _touchCubesParent.SetActive(false);
+
+        _touchCubes = new GameObject[3];
+        for (int i = 0; i < 3; i++) {
+            _touchCubes[i] = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            _touchCubes[i].transform.SetParent(_touchCubesParent.transform);
+            _touchCubes[i].transform.localPosition = Vector3.forward * (i - 1) * touchCubeOffset;
+            _touchCubes[i].transform.localRotation = Quaternion.identity;
+            _touchCubes[i].transform.localScale = new Vector3(0, 0, 0);
+        }
 
         textMesh.SetText("To begin, press one of the red buttons at your feet (Watch your head!)");
     }
@@ -65,6 +86,9 @@ public class Table : MonoBehaviour {
             case State.BugGameSpawn:
                 BugGameSpawn();
                 break;
+            case State.Redirect:
+                Redirect();
+                break;
             default:
                 break;
         }
@@ -75,7 +99,41 @@ public class Table : MonoBehaviour {
             _bugs.Remove(bug);
             _bugScore++;
             textMesh.SetText("Smash all of the bugs!\nScore: " + _bugScore);
+
+            if (_bugs.Count == 0) {
+                state = State.Redirect;
+                textMesh.SetText("Pinch to set the size and position of the real object");
+            }
         }
+    }
+
+    private void SetTouchCubeSize(OVRHand hand) {
+        if (!hand.GetFingerIsPinching(OVRHand.HandFinger.Index))
+            return;
+        float minY = float.PositiveInfinity;
+        bool handInArea = false;
+        CapsuleCollider[] colliders = hand.GetComponentsInChildren<CapsuleCollider>();
+        foreach (CapsuleCollider collider in colliders) {
+            if (_collider.bounds.Intersects(collider.bounds))
+                handInArea = true;
+            minY = Mathf.Min(minY, collider.bounds.min.y);
+        }
+        if (handInArea && minY != float.PositiveInfinity) {
+            if (!_touchCubesParent.activeSelf)
+                _touchCubesParent.SetActive(true);
+            _touchCubesParent.transform.position = new Vector3(
+                hand.transform.position.x,
+                (minY + transform.position.y) / 2,
+                hand.transform.position.z
+            );
+            for (int i = 0; i < 3; i++)
+                _touchCubes[i].transform.localScale = Mathf.Abs(transform.position.y - minY) * Vector3.one;
+        }
+    }
+
+    private void Redirect() {
+        SetTouchCubeSize(lHand);
+        SetTouchCubeSize(rHand);
     }
 
     private void BugGameSpawn() {
@@ -90,10 +148,10 @@ public class Table : MonoBehaviour {
             Bug bug = Instantiate<Bug>(bugPrefab, transform);
             bug.transform.Rotate(0, Random.Range(0f, 360f), 0);
             bug.transform.localPosition = new Vector3(
-                Random.Range(-0.5f, 0.5f) * _cube.transform.localScale.x,
+                Random.Range(-0.5f, 0.5f) * _tableCube.transform.localScale.x,
                 thickness / 2,
-                Random.Range(-0.5f, 0.5f) * _cube.transform.localScale.z
-            );                        
+                Random.Range(-0.5f, 0.5f) * _tableCube.transform.localScale.z
+            );
             bug.table = this;
             bug.xBounds = (_maxX - _minX) / 2;
             bug.zBounds = (_maxZ - _minZ) / 2;
@@ -103,7 +161,7 @@ public class Table : MonoBehaviour {
     }
 
     private void Rescale() {
-        SetCubeSize();
+        SetTableCubeSize();
 
         if (!_button.activeSelf &&
                 (!lHand.IsTracked ||
@@ -114,7 +172,7 @@ public class Table : MonoBehaviour {
         }
     }
 
-    private void SetCubeSize() {
+    private void SetTableCubeSize() {
         _minX = float.PositiveInfinity;
         _minZ = float.PositiveInfinity;
         _maxX = float.NegativeInfinity;
@@ -128,7 +186,8 @@ public class Table : MonoBehaviour {
             _maxZ = Mathf.Max(_anchors[i].transform.localPosition.z, _maxZ);
         }
         transform.Translate((_maxX + _minX) / 2, 0, (_maxZ + _minZ) / 2);
-        _cube.transform.localScale = new Vector3(_maxX - _minX, thickness, _maxZ - _minZ);
+        _tableCube.transform.localScale = new Vector3(_maxX - _minX, thickness, _maxZ - _minZ);
+        _collider.size = new Vector3(_maxX - _minX, 0.5f, _maxZ - _minZ);
         for (int i = 0; i < _anchors.Length; i++) {
             _anchors[i].transform.position = positions[i];
             _anchors[i].transform.localPosition = Vector3.Project(_anchors[i].transform.localPosition, _anchors[i].scaleVec);
